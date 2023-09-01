40808ef6cd0e5cd52ee12cd98c57a1c91fd3569e
Navor Iturvi <navoriturvi34@gmail.com>
2023-08-31 21:16:32 -0400

    Primer commit, se crea el gitignore, 2 modelos principales y se inician proyecto y app de django

---
+++ to_do/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    placed_date = models.DateField(auto_now_add=True)
+    dead_line =models.DateField()
+    group = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+    placed_date = models.DateField(auto_now_add=True)
+    dead_line = models.DateField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
